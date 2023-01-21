bd6ade6d93ad8f13a338b07a21452da6e326b124
mazharm <mazharm@hotmail.com>
2023-01-20 21:49:54 -0800

    More updates to fix pylint errors.

--- life.py
+++ life.py
@@ -1,3 +1,7 @@
+"""
+A simple implementation of Game of Life with fluid implementation of
+board setup
+"""
 import sys
 import pygame
 import numpy as np
@@ -14,12 +18,13 @@ DEAD_COLOR = (0, 0, 0)
 TRANSPARENT_COLOR = (0, 0, 0)
 
 # Cell size in pixels
-CELL_SIZE = 8
+CELL_SIZE = 10
 
-"""
-Apply the rules of the Game of Life to the board
-"""
+
 def game_of_life(board):
+    """
+    Apply the rules of the Game of Life to the board
+    """
     # Copy the board to a new array to avoid modifying the original
     new_board = np.copy(board)
     for i in range(1, board.shape[0]-1):
@@ -36,27 +41,34 @@ def game_of_life(board):
     return new_board
 
 def draw_board(screen, board, previous_board):
-    for x in range(board.shape[0]):
-        for y in range(board.shape[1]):
-            if board[x, y] != previous_board[x, y]:
-                if board[x, y]:
+    """
+    Render the board to the screen
+    """
+    for board_x in range(board.shape[0]):
+        for board_y in range(board.shape[1]):
+            if board[board_x, board_y] != previous_board[board_x, board_y]:
+                if board[board_x, board_y]:
                     color = ALIVE_COLOR
                 else:
                     color = DEAD_COLOR
-                rect = (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+                rect = (board_x * CELL_SIZE, board_y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
                 pygame.draw.rect(screen, color, rect)
                 pygame.display.update(rect)
     previous_board[:] = board
 
 def draw_buttons(screen, running, draw) :
+    """ Render the Draw/Erase, Reset and Start/Pause buttons on the screen
+    """
     # Set up font for button text
     font = pygame.font.Font(None, 30)
 
     # Create the draw button -- this is only visible when the game is not running
-    draw_button = pygame.Rect((WIDTH - BUTTON_WIDTH)//2, HEIGHT - 3*BUTTON_HEIGHT-20, BUTTON_WIDTH, BUTTON_HEIGHT)
+    draw_button = pygame.Rect((WIDTH - BUTTON_WIDTH)//2, HEIGHT - 3*BUTTON_HEIGHT-20,\
+                               BUTTON_WIDTH, BUTTON_HEIGHT)
 
     # Create the reset button -- this is only visible when the game is not running
-    reset_button = pygame.Rect((WIDTH - BUTTON_WIDTH)//2, HEIGHT - 2*BUTTON_HEIGHT-10, BUTTON_WIDTH, BUTTON_HEIGHT)
+    reset_button = pygame.Rect((WIDTH - BUTTON_WIDTH)//2, HEIGHT - 2*BUTTON_HEIGHT-10,\
+                                BUTTON_WIDTH, BUTTON_HEIGHT)
 
     blank_text = font.render("********", True, DEAD_COLOR, (0,0,0,0))
 
@@ -76,30 +88,38 @@ def draw_buttons(screen, running, draw) :
         draw_text = font.render(draw_title, True, (255,255,255), (0,0,0,0))
         color = (255, 0, 0)
 
-    screen.blit(reset_text, (reset_button.x + (reset_button.width - reset_text.get_width()) // 2, reset_button.y + (reset_button.height - reset_text.get_height()) // 2))
+    screen.blit(reset_text, (reset_button.x + (reset_button.width - reset_text.get_width()) \
+                 // 2, reset_button.y + (reset_button.height - reset_text.get_height()) // 2))
     pygame.draw.rect(screen, color, reset_button, 2)
 
-    screen.blit(draw_text, (draw_button.x + (draw_button.width - draw_text.get_width()) // 2, draw_button.y + (draw_button.height - draw_text.get_height()) // 2))
+    screen.blit(draw_text, (draw_button.x + (draw_button.width - draw_text.get_width()) \
+                 // 2, draw_button.y + (draw_button.height - draw_text.get_height()) // 2))
     pygame.draw.rect(screen, color, draw_button, 2)
 
     # Create the start/pause game button -- this is button is always visible
-    start_game_button = pygame.Rect((WIDTH - BUTTON_WIDTH)//2, HEIGHT - BUTTON_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT)
+    start_game_button = pygame.Rect((WIDTH - BUTTON_WIDTH)//2, HEIGHT - BUTTON_HEIGHT, \
+                            BUTTON_WIDTH, BUTTON_HEIGHT)
 
     start_game_text = font.render(game_button_name, True, (255,255,255), (0,0,0,0))
     # draw the blank surface over the previous text
-    screen.blit(start_game_text, (start_game_button.x + (start_game_button.width - start_game_text.get_width()) // 2, start_game_button.y + (start_game_button.height - start_game_text.get_height()) // 2))
+    screen.blit(start_game_text, (start_game_button.x + \
+                (start_game_button.width - start_game_text.get_width()) // 2, \
+                start_game_button.y + (start_game_button.height - \
+                start_game_text.get_height()) // 2))
     pygame.draw.rect(screen, (255, 0, 0), start_game_button, 2)
 
     return start_game_button, reset_button, draw_button
 
 def main():
+    """ Main function"""
+
     # Initialize pygame
     pygame.init()
 
     # Create a blank board
     game_board = np.zeros((80, 80), dtype=bool)
     clock = pygame.time.Clock()
-    FPS = 60
+    f_p_s = 60
     mouse_down = False
     draw = True
     running = False
@@ -140,16 +160,16 @@ def main():
             game_board = game_of_life(game_board)
         if mouse_down:
             # draw/erase a cell
-            x, y = pygame.mouse.get_pos()
-            x, y = x // CELL_SIZE, y // CELL_SIZE
-            if x < game_board.shape[0] and y < game_board.shape[1]:
-                game_board[x, y] = draw
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            board_x, board_y = mouse_x // CELL_SIZE, mouse_y // CELL_SIZE
+            if board_x < game_board.shape[0] and board_y < game_board.shape[1]:
+                game_board[board_x, board_y] = draw
 
         # Draw the board
         draw_board(game_screen, game_board, previous_board)
 
         pygame.display.flip()
-        clock.tick(FPS)
+        clock.tick(f_p_s)
 
 
 if __name__ == "__main__":
